aacf9fb1a07ad1a8c4388803e64fcc7277951a5c
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-05-30 20:01:14 +0300

    Fix(JSX) Update styles

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -5,11 +5,7 @@ export const List = styled.ul`
   max-width: calc(100vw - 48px);
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 16px;
-  margin-top: 0;
-  margin-bottom: 0;
-  padding: 0;
+  margin: 0 auto;
+  padding: 84px 0 0 0;
   list-style: none;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 100px;
 `;
